3e3619b3ca79012a44405a404d0fb7fb4427c17c
Charlize C <charlizechang.5in@gmail.com>
2026-05-27 15:53:23 +0800

    Draw

--- students/Charlize/scripts/graffiti.js
+++ students/Charlize/scripts/graffiti.js
@@ -28,16 +28,38 @@ surface.lineTo(270, 300);
 
 /*shapes*/
 
-function shapes (){
-    surface.rect(150, 200, 100 ,100);
+function shapes() {
+    surface.rect(150, 200, 100, 100);
     surface.stroke();
 
 }
 shapes();
 
 /*Clean*/
-function Clean(){
+function Clean() {
     console.log('Clean');
-surface.clearRect(0, 0, 400, 400);
+    surface.clearRect(0, 0, 400, 400);
 }
 cleanButton.addEventListener("click", Clean);
+
+
+/*Graffiti */
+let oldX = 0;
+let oldY = 0;
+function graffiti(event) {
+
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y, event.buttons);
+    if (event.buttons === 1){
+    
+    surface.beginPath();
+    surface.moveTo(oldX, oldY);
+    surface.lineTo(x, y);
+    surface.closePath();
+    surface.stroke();
+
+    oldX = x;
+    oldY = y;
+}}
+graffitiCanvas.addEventListener('mousemove', graffiti);
