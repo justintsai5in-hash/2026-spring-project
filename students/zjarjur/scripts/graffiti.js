93eb586287bd21ebea29f61bf05285c2740684c4
ZJarjur <zjarjur@gmail.com>
2026-05-27 15:56:15 +0800

    Draw Graffiti

--- students/zjarjur/scripts/graffiti.js
+++ students/zjarjur/scripts/graffiti.js
@@ -24,11 +24,11 @@ function shapes() {
     surface.rect(150, 200, 100, 100);
     surface.stroke();
     surface.beginPath();
-    surface.moveTo(200,50);
-    surface.lineTo(150,200);
+    surface.moveTo(200, 50);
+    surface.lineTo(150, 200);
     surface.moveTo(250, 50);
-    surface.lineTo(350,50);
-    surface.lineTo(300,300); 
+    surface.lineTo(350, 50);
+    surface.lineTo(300, 300);
     surface.stroke();
 
 }
@@ -40,8 +40,36 @@ shapes();
 */
 
 function cleanCanvas() {
-    surface.clearRect(0,0,400,400);
+    surface.clearRect(0, 0, 400, 400);
     console.log("Clean");
 }
 
 cleanButton.addEventListener("click", cleanCanvas);
+
+
+/*
+* Graffiti
+*/
+let oldX = 0;
+let oldY = 0;
+
+function graffiti(event) {
+
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y, event.buttons);
+
+    if (event.buttons > 0){
+    surface.beginPath();
+    surface.moveTo(oldX, oldY);
+    surface.lineTo(x, y);
+    surface.closePath();
+    surface.stroke();
+    }
+
+    oldX = x;
+    oldY = y;
+
+}
+
+graffitiCanvas.addEventListener("mousemove", graffiti);
