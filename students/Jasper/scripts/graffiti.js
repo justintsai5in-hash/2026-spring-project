d274019bf95ba85dcf8a929b6713bff01b32b16c
jasper <jasperliu.5in@gmail.com>
2026-05-27 15:51:41 +0800

    graffiti

---
+++ students/Jasper/scripts/graffiti.js
@@ -0,0 +1,62 @@
+/*
+ * HTML elements
+ */
+const graffitiCanvas = document.getElementById("my-graffiti");
+const surface = graffitiCanvas.getContext("2d");
+const cleanButton = document.getElementById("clean");
+
+/*
+ * Graffiti style.
+ */
+surface.lineWidth = 13;
+surface.lineJoin = "round";
+surface.strokeStyle = "red";
+
+/*
+ * Shapes
+ */
+function shapes() {
+  surface.rect(150, 200, 100, 100);
+  surface.stroke();
+
+  surface.beginPath();
+  surface.moveTo(200, 50);
+  surface.lineTo(150, 200);
+  // Add a 7.
+  surface.moveTo(250, 50);
+  surface.lineTo(350, 50);
+  surface.moveTo(350, 50);
+  surface.lineTo(270, 300);
+
+  surface.closePath();
+  surface.stroke();
+}
+shapes();
+
+/*
+ * Clean up
+ */
+function cleanCanvas() {
+  surface.clearRect(0, 0, 400, 400);
+}
+    oldX = 0
+    oldY = 0
+function graffiti(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y, event.buttons)
+    
+    if(event.buttons === 1){
+    surface.beginPath();
+    surface.moveTo(oldX, oldY);
+    surface.lineTo(x, y);
+    surface.closePath();
+    surface.stroke();      
+    }
+
+    oldX = x
+    oldY = y
+}
+
+cleanButton.addEventListener("click", cleanCanvas);
+graffitiCanvas.addEventListener("mousemove", graffiti)
